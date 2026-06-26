78bfee19c8a92e5d5bbff312e1095415a6a2367a
Luke <lukebisgard@gmail.com>
2026-06-25 20:57:01 -0700

    Added direction for module 2 assignments

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'Hello World from Laravel Herd!';
 });
+
+Route::get('/module2a/price_engine_refactored.php', function () {
+    require base_path('module2a/price_engine_refactored.php');
+});
+
+Route::get('/module2b/cosmic_calendar.php', function () {
+    require base_path('module2b/cosmic_calendar.php');
+});
